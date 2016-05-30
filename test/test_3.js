7ba619d54ee8a48ce2aa188e84174be1dbdfb495
Julius Reade <Julius_dott@hotmail.com>
2016-05-31 08:34:51 +1000

    initial commit

---
+++ test/test_3.js
@@ -0,0 +1,64 @@
+// Add move right functionality 
+// Modularise program 
+// Grid should be 41h by 801w, so it's just single lane. 
+
+var xpos = 0;
+var ypos = 0;
+var snake = document.getElementById("snake");
+
+console.log(xpos);
+console.log(ypos);
+
+function snakeMovementRight() { 
+	
+	var id = setInterval(moveright, 10);
+
+	function moveright() {
+		if (xpos === 760 || 40) {
+			clearInterval(id);
+		} else {
+			xpos--;
+			snake.style.left = pos + 'px'; // left is right and vice-versa. 
+		}	
+	}
+}
+
+function snakeMovementLeft() { 
+	var id = setInterval(moveleft, 5);
+
+	function moveleft() {
+		if (xpos === 760) {
+			clearInterval(id);
+		} else {
+			xpos--;
+			snake.style.left = xpos + 'px';
+		}
+	}
+}
+
+function snakeMovementRight() { 
+	var id = setInterval(moveright, 5);
+
+	function moveright() {
+		if (xpos === 760) {
+			clearInterval(id);
+		} else {
+			xpos++;
+			snake.style.right = xpos + 'px';
+		}
+	}
+}
+
+
+function snakeMovementEvent(event) {
+	console.log(event.keyCode)
+	if (event.keyCode === 115) {
+		snakeMovementRight();
+	} else if (event.keyCode === 97) {
+		snakeMovementLeft();
+	} else {
+		return
+	}
+}
+
+addEventListener('keypress', snakeMovementEvent);
